890a0fac0575f128ebbf2a61f0a26792791cc05a
AlenaDemon <drmkpr@mail.ru>
2025-01-30 18:47:52 +0400

    logic

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,23 +1,13 @@
-import readlineSync from 'readline-sync';
-import { generateNumber } from '../index.js';
+import logicGames from '../index.js';
 
-export default () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let i = 0;
-  while (i < 3) {
-    const number = generateNumber(1, 40);
-    const trueAnswer = (number % 2 === 0) ? 'yes' : 'no';
-    const qest = readlineSync.question(`Question: ${number}\nYour answer: `);
-    if (qest === trueAnswer) {
-      i += 1;
-      console.log('Correct!');
-    } else {
-      console.log(`'${qest}' is wrong answer ;(. Correct answer was '${trueAnswer}'.\nLet's try again, ${userName}!`);
-      process.exit();
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+const generateNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const create = () => {
+  const expression = generateNumber(1, 40);
+  const trueAnswer = (expression % 2 === 0) ? 'yes' : 'no';
+  return { question: expression, trueAnswer };
 };
+const evenGame = () => {
+  logicGames(create, rules);
+};
+export default evenGame;
--- src/index.js
+++ src/index.js
@@ -1,75 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const greetings = (condition) => {
+const logicGames = (create, rules) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  console.log(condition);
-};
-
-const generateNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-const expression = () => {
-  const num1 = generateNumber(1, 40);
-  const num2 = generateNumber(1, 40);
-  const operationsArr = ['+', '-', '*'];
-  const operations = operationsArr[generateNumber(0, 2)];
-  return `${num1} ${operations} ${num2}`;
-};
-
-const resultExpression = (str) => {
-  const cat = str.split(' ');
-  let result;
-  if (cat.includes('+')) {
-    result = Number(cat[0]) + Number(cat[2]);
-  } else if (cat.includes('-')) {
-    result = Number(cat[0]) - Number(cat[2]);
-  } else if (cat.includes('*')) {
-    result = Number(cat[0]) * Number(cat[2]);
+  console.log(rules);
+  let i = 0;
+  while (i < 3) {
+    const result = create();
+    console.log(`Question: ${result.question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === result.trueAnswer) {
+      i += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${result.trueAnswer}'.\nLet's try again, ${userName}!`);
+      process.exit();
+    }
   }
-  return result;
+  console.log(`Congratulations, ${userName}!`);
 };
-
-const mostCommonDivisor = (str) => {
-  const num = str.split(' ');
-  let a = Number(num[0]);
-  let b = Number(num[1]);
-  while (b !== 0) {
-    const temp = b;
-    b = a % b;
-    a = temp;
-  }
-  return a;
-};
-
-const progressionArr = () => {
-  const first = generateNumber(1, 10);
-  const i = generateNumber(1, 10);
-  const arr = [];
-  arr.push(first);
-  while (arr.length < 10) {
-    const second = Number(arr.at(-1)) + i;
-    arr.push(second);
-  }
-
-  return arr;
-};
-const hiddensqElement = (arr) => {
-  const num = generateNumber(0, 9);
-  const numTrue = arr[num];
-  const arrNew = arr;
-  arrNew[num] = '..';
-  return [arrNew, [numTrue]];
-};
-
-const isPrime = (num) => {
-  for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return false;
-  }
-  return num !== 1;
-};
-
-export {
-  expression, resultExpression, generateNumber, mostCommonDivisor, progressionArr, hiddensqElement,
-  greetings, isPrime,
-};
+export default logicGames;
